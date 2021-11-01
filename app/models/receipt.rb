87de95d2eaaee89177b6c4a8242750d01bf2685a
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-10-31 22:07:28 -0600

    Complete calculation methods for receipts.

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -4,31 +4,49 @@ class Receipt < ApplicationRecord
   has_many  :registers
   has_many  :items, through: :registers
 
-  def discounted_total_by_item(scanned_items)
-    # add full price total for savings?
-    item_totals = Hash.new
-    sorted_items = sort_items(scanned_items)
+  def totals(scanned_items)
     discounts = Discount.all
-    sorted_items.each do |item|
-      discounts.each do |discount|
-        # helepr method start
-        if item[1] >= discount.qty_required
-          item_discount = discount.discounted_price
-          item_full_price = item[0][:price]
-          apply_discount_times = item[1] / discount.qty_required
-          apply_full_price_times = item[1] % discount.qty_required
-          discount_total = item_discount * apply_discount_times
-          full_price_total = item_full_price * apply_full_price_times
-          item_total = discount_total + full_price_total
-        else
-          item_full_price = item[0][:price]
-          apply_full_price_times = item[1] % discount.qty_required
-          item_total = item_full_price * apply_full_price_times
-        end
-          item_totals[item[0][:name]] = item_total
+    purchased_items = sort_items(scanned_items)
+    items_stats = Hash.new
+    purchased_items.each do |item|
+      name = item[0][:name]
+      discount = discounts.where(name: name)
+      if discount == [] || item[1] < discount[0][:qty_required]
+        total = item[1] * item[0][:price]
+        items_stats[item[0][:name]] = {transaction_total: total, without_disc: total, item_savings: 0}
+      else discount && item[1] >= discount[0][:qty_required]
+        discounted_total = (item[1] / discount[0][:qty_required]) * discount[0][:discounted_price]
+        full_price_total = (item[1] % discount[0][:qty_required]) * item[0][:price]
+        total = discounted_total + full_price_total
+        items_stats[name] = {transaction_total: total, without_disc: (item[0][:price] * item[1]).round(2), item_savings: (item[0][:price] * item[1] - total).round(2)}
       end
     end
-    item_totals
+    items_stats
+    # add full price total for savings?
+    # item_totals = Hash.new
+    # sorted_items = sort_items(scanned_items)
+    # discounts = Discount.all
+    # discounts.each do |discount|
+    #   sorted_items.each do |item|
+        # helepr method start
+    #     if item[1] >= discount.qty_required
+    #       item_discount = discount.discounted_price
+    #       item_full_price = item[0][:price]
+    #       apply_discount_times = item[1] / discount.qty_required
+    #       apply_full_price_times = item[1] % discount.qty_required
+    #       discount_total = item_discount * apply_discount_times
+    #       full_price_total = item_full_price * apply_full_price_times
+    #       item_total = discount_total + full_price_total
+    #       item_totals[item[0][:name]] = item_total
+    #     else
+    #       item_full_price = item[0][:price]
+    #       apply_full_price_times = item[1]
+    #       item_total = item_full_price * apply_full_price_times
+    #       item_totals[item[0][:name]] = item_total
+    #     end
+    #   end
+    # end
+    # item_totals
   end
 
   def sort_items(items)
--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -15,11 +15,25 @@ RSpec.describe Receipt, type: :model do
       receipt = Receipt.create(transaction_time: Time.now)
       milk = Item.create(name: 'Milk', price: 3.97)
       bread = Item.create(name: 'Bread', price: 2.17)
+      banana = Item.create(name: 'Banana', price: 0.99)
+      apple = Item.create(name: 'Apple', price: 0.89)
       receipt.scan_item(milk)
       receipt.scan_item(milk)
       receipt.scan_item(bread)
-      milk_money = milk.discounts.create(name: 'Milk', qty_required: 2, discounted_price:5.00)
-      expect(receipt.discounted_total_by_item(receipt.items)).to eq({'Milk' => 5.00, 'Bread' => 2.17})
+      receipt.scan_item(bread)
+      receipt.scan_item(bread)
+      receipt.scan_item(bread)
+      receipt.scan_item(apple)
+      receipt.scan_item(banana)
+      milk_money = milk.discounts.create(name: 'Milk', qty_required: 2, discounted_price: 5.00)
+      bakers_bonanza = bread.discounts.create(name: 'Bread', qty_required: 3, discounted_price: 6.00)
+      gone_bananas = banana.discounts.create(name: 'Banana', qty_required: 2, discounted_price: 0.50)
+      result = {
+        "Apple"=>{:transaction_total=> 0.89, :without_disc=>0.89, :item_savings=>0},
+        "Milk"=>{:transaction_total=>5.0, :without_disc=>7.94, :item_savings=>2.94},
+        "Bread"=>{:transaction_total=>8.17, :without_disc=>8.68, :item_savings=>0.51},
+        "Banana"=>{:transaction_total=>0.99, :without_disc=>0.99, :item_savings=>0}}
+      expect(receipt.totals(receipt.items)).to eq(result)
     end
 
     it 'can sort items' do
